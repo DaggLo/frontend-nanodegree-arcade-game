28ea5f953c01bbd8a04494d328a5d48e48f1ba39
Evgeny Degtev <e.a.degtev@gmail.com>
2015-11-27 01:25:11 +0300

    feat: Add instances. Fix some bugs.
    Fix bugs with the enemies and player movements.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// Enemies our player must avoid
+﻿// Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
@@ -7,13 +7,13 @@ var Enemy = function() {
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
     this.loc = [
-            0,
+            -101,
             function() {
-                return ( Math.floor( Math.random() * 4 ) + 1 ) * 83;
+                return ( Math.floor( Math.random() * 3 )  + 1) * 83 - 25;
             }()
         ];
     this.speed = function() {
-        return Math.floor( Math.random() * 3 ) + 1;
+        return (Math.floor( Math.random() * 3 ) + 1) * 100;
     }();
 };
 
@@ -23,7 +23,17 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.loc[0] = (this.loc[0] + this.speed) * dt;
+    if (this.loc[0] < 505) {
+	    this.loc[0] += this.speed * dt;
+	
+	} else {
+	    this.loc = [
+            -101,
+            function() {
+                return ( Math.floor( Math.random() * 3 )  + 1) * 83 - 25;
+            }()
+        ];
+	}
 };
 
 // Draw the enemy on the screen, required method for game
@@ -36,36 +46,49 @@ Enemy.prototype.render = function() {
 // a handleInput() method.
 var Player = function() {
     this.sprite = 'images/char-boy.png';
-    this.loc = [202, 415];
+	
+    this.loc = [202, 404];
+	
     this.update = function(x, y) {
-        this.loc[0] += x;
-        this.loc[1] += y;
+        if (x !== undefined && y !== undefined) {
+		    this.loc[0] += x;
+            this.loc[1] += y;
+			
+		} else return;
     };
-    this.render = ;
+	
+    this.render = function() {
+	    ctx.drawImage(Resources.get(this.sprite), this.loc[0], this.loc[1]);
+	};
+		
     this.handleInput = function(key) {
-        if (key === "left" && this.loc[0] >= 101) {
-            this.update(-100, 0);
+        if (key == "left" && this.loc[0] >= 101) {
+            this.update(-101, 0);
         }
 
-        if (key === "up" && this.loc[1] <= 404) {
-            this.update(0, -100);;
+        if (key == "up" && this.loc[1] >= 50) {
+            this.update(0, -83);
         }
 
-        if (key === "right") {
-            this.loc[0] = this.loc[0] + 101;
+        if (key == "right" && this.loc[0] <= 303) {
+            this.update(101, 0);
         }
 
-        if (key === "down") {
-            this.loc[0] = this.loc[0] + 83;
-        }
+        if (key == "down" && this.loc[1] <= 330) {
+            this.update(0, 83);
+			
+        } else {
+		    this.update(0, 0);
+		}
     };
 };
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = new Array(new Enemy(), new Enemy(), new Enemy(), new Enemy());
 
-
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -79,3 +102,5 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+// TODO: https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
